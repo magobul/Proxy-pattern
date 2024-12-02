01775da088c9abed6b237955e398fa23e5aff999
magobul <magobul@gmail.com>
2024-12-02 11:55:25 +0700

    Program.cs

--- Proxy pattern/Proxy pattern/Program.cs
+++ Proxy pattern/Proxy pattern/Program.cs
@@ -6,16 +6,16 @@ class Program
     {
         Proxy proxy = new Proxy(TimeSpan.FromSeconds(10));
 
-        // Первый запрос - будет обработан RealSubject
+        // Первый запрос обработан RealSubject
         Console.WriteLine(proxy.Request("Запрос 1"));
 
-        // Второй запрос - будет возвращен из кэша
+        // Второй запрос возвращен из кэша
         Console.WriteLine(proxy.Request("Запрос 1"));
 
         // Ждем 11 секунд, чтобы кэш истек
         Thread.Sleep(11000);
 
-        // Запрос снова будет обработан RealSubject
+        // Запрос снова обработан RealSubject
         Console.WriteLine(proxy.Request("Запрос 1"));
     }
 }
